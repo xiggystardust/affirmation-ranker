fcd7956f1efdfbff7643d23a008c8050679c7836
Sarah Burke Spolaor <sarahbspolaor@gmail.com>
2026-04-20 22:57:48 -0400

    testing new survey deployment

--- src/pages/RankingPage.jsx
+++ src/pages/RankingPage.jsx
@@ -6,7 +6,7 @@ import RankingList from '../components/RankingList'
 import { useAffirmations } from '../hooks/useAffirmations'
 import { submitRankingResults } from '../services/resultsService'
 
-const SURVEY_ID = 'march-2026'
+const SURVEY_ID = 'my-test'
 
 function RankingPage() {
   const navigate = useNavigate()
